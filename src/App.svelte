550f37a502fe8cec6b34c69eefc6f64e074e9542
xuan <s986309@gmail.com>
2021-03-29 12:24:15 +0800

    Add function log, stage and distory. Fix aspect ratio. Update init_info.

--- src/App.svelte
+++ src/App.svelte
@@ -8,60 +8,67 @@
     let canvas_obj: any;
 
     let trigger_val = 1;
-    /*let trigger = (f) => {*/
-        /*return () => {*/
-            /*f();*/
-            /*console.log('HO');*/
-            /*trigger_val += 1;*/
-            /*if (trigger_val > 1000)*/
-                /*trigger_val = 1;*/
-        /*}*/
-    /*}*/
 
-    let land_profile = [];
-    let land_sel = 8;
-    let rail_profile = [];
-    let rail_sel = 2;
+    let nstage        = 0;
+    let stage         = [];
+    let land_profile  = [];
+    let rail_profile  = [];
     let infra_profile = [];
-    let infra_sel = 2;
+    let land_sel;
+    let rail_sel;
+    let infra_sel;
 
     let budget     = 0;
     let budget_r   = 0;
     let nconv      = 0;
     let nconv_r    = 0;
+    let est_cost   = 0;
+    let est_nconv  = 0;
     let land_price = 0;
     let state      = 0;
+    let stage_count= 0;
+    let log_str    = "";
     $: if (trigger_val) {
         if (map !== undefined) {
-            map.comp_land_price();
-            budget     = map.budget;
-            budget_r   = map.budget_r;
-            nconv      = map.nconv;
-            nconv_r    = map.nconv_r;
-            land_price = map.land_price;
+            budget        = map.budget;
+            budget_r      = map.budget_r;
+            nconv         = map.nconv;
+            nconv_r       = map.nconv_r;
+            land_price    = map.land_price;
+            stage_count   = map.stage_count;
+            land_profile  = stage[stage_count].land_profile;
+            rail_profile  = stage[stage_count].rail_profile;
+            infra_profile = stage[stage_count].infra_profile;
         }
         if (canvas !== undefined) {
             canvas.draw();
-            state = canvas.state;
+            state   = canvas.state;
+            log_str = canvas.get_log();
+            const res = canvas.comp_cost(state == 0? land_sel: state == 1? rail_sel: infra_sel);
+            est_cost  = res.cost;
+            est_nconv = res.nconv;
         }
         if (trigger_val > 1000)
             trigger_val = 1;
     }
 
-    onMount(() => init())
-
     async function init() {
         const res       = await fetch('./init.json');
         const init_info = await res.json();
 
-        land_profile = init_info.land_profile;
-        rail_profile = init_info.rail_profile;
-        infra_profile = init_info.infra_profile;
-        map    = new Map(init_info);
+        nstage = init_info.nstage;
+        stage  = init_info.stage;
+        map    = new Map(init_info.map,
+                         init_info.nstage,
+                         init_info.stage);
         canvas = new Canvas(canvas_obj, 
                             (() => {trigger_val += 1}),
                             map);
+        land_sel  = stage[0].land_profile.findIndex((x) => x.tax > 0);
+        rail_sel  = stage[0].rail_profile.findIndex((x) => x.tax > 0);
+        infra_sel = stage[0].infra_profile.findIndex((x) => x.tax > 0);
     }
+    onMount(() => init())
 
 </script>
 
@@ -75,7 +82,7 @@
     </div>
     <div style="float: right; text-align:left;">
         <h1> TAKAO Fa Da Cai !</h1>
-        <h2> v0.3</h2>
+        <h2> v0.3.2</h2>
 
         <table>
             <tr>
@@ -93,11 +100,30 @@
                 <th> {nconv_r}/{nconv} </th>
             </tr>
             <tr>
+                <th> 預估花費 </th>
+                <th> {est_cost.toFixed(1)} </th>
+            </tr>
+            <tr>
+                <th> 預估變更 </th>
+                <th> {est_nconv} </th>
+            </tr>
+            <!---<tr>
                 <th> 公告現值： </th>
                 <th> {land_price.toFixed(1)} </th>
-            </tr>
+            </tr>--->
         </table>
         <table>
+            <tr>
+                <th colspan=2>
+                    <p>
+                    {stage_count + 1} <del>{stage_count == 0? "黑暗": 
+                                            stage_count == 1? "封建": 
+                                            stage_count == 2? "城堡": 
+                                                              "發大財"}時代</del>
+                    </p>
+                </th>
+                <th><button disabled={stage_count==nstage-1} on:click={() => canvas.update_stage()}>升級</button></th>
+            </tr>
             <tr>
                 <th><button on:click={() => canvas.set_state(0)}>土地分區</button></th>
                 <th>
@@ -138,7 +164,22 @@
                 <th><button disabled={state != 2} on:click={() => canvas.build(infra_sel)}>建設</button></th>
             </tr>
             <tr>
+                <th><button on:click={() => canvas.set_state(3)}>拆除網絡</button></th>
+                <th> </th>
+                <th><button disabled={state != 3} on:click={() => canvas.build(infra_sel)}>拆！</button></th>
+            </tr>
+            <tr>
+                <th><button on:click={() => canvas.set_state(4)}>拆除建設</button></th>
+                <th> </th>
+                <th><button disabled={state != 4} on:click={() => canvas.build(infra_sel)}>拆！</button></th>
+            </tr>
+            <tr>
                 <th><button on:click={() => canvas.set_state(canvas.state)}>清空選取</button></th>
+            </tr>
+            <tr>
+                <th colspan="3">
+                    <textarea style={'visibility:' + (stage_count==nstage-1? 'visible': 'hidden')} rows="4"> {log_str} </textarea>
+                </th>
             </tr>
             <tr>
                 <th>
@@ -149,7 +190,3 @@
         </table>
     </div>
 </main>
-
-<style>
-</style>
-
--- src/map.ts
+++ src/map.ts
@@ -12,12 +12,37 @@ enum State {
     Land  = 0,
     Rail  = 1,
     Infra = 2,
+    DelR  = 3,
+    DelI  = 4,
+    Stage = 5,
 };
 
 const sqrt3: number     = 1.732050808;
 const sqrt3_2: number   = sqrt3 / 2.;
 const delta_x: number[] = [0., sqrt3_2, sqrt3_2, 0., -sqrt3_2, -sqrt3_2];
 const delta_y: number[] = [1., 0.5,     -0.5,   -1., -0.5,     0.5];
+
+function grid_to_hex_center (coord: HCoord): Coord {
+    const x: number = sqrt3 * coord.c + sqrt3 + sqrt3_2 * (coord.r % 2);
+    const y: number = 1.5   * coord.r + 1.5;
+    return {'x': x, 'y': y};
+}
+
+function HCoord_eq (x: HCoord, y: HCoord): boolean {
+    return x.r == y.r && x.c == y.c;
+}
+
+//v log
+//v stage
+//v aspect rate
+//v destroy
+//show
+//cookie
+//v ui
+// TRA  Blue
+// MRT  Yellow
+// LR   Green
+// Road Red
 
 // These classes save the properties of each land
 class Rail {
@@ -49,6 +74,10 @@ export class Map {
     land:          number[];
     rail:          Rail[];
     infra:         Infra[];
+
+    nstage:        number;
+    stage_count:   number;
+    stage:         any[];
     land_profile:  any;
     rail_profile:  any;
     infra_profile: any;
@@ -59,29 +88,39 @@ export class Map {
     nconv_r:    number;
     land_price: number;
 
-    constructor(init_info: {'num_c':  number,
-                            'num_r':  number,
-                            'budget': number,
-                            'nconv':  number,
-                            'land':   number[][],
-                            'rail':   Rail[],
-                            'infra':  Infra[],
-                            'land_profile': any,
-                            'rail_profile': any,
-                            'infra_profile': any}) {
-        this.num_c         = init_info.num_c;
-        this.num_r         = init_info.num_r;
-        this.budget        = init_info.budget;
-        this.budget_r      = init_info.budget;
-        this.nconv         = init_info.nconv;
-        this.nconv_r       = init_info.nconv;
-        this.land          = new Array(this.num_r * this.num_c);
-        this.for_any_idx((coord, x) => this.land[x] =  init_info.land[coord.r][coord.c]);
-        this.rail          = init_info.rail;
-        this.infra         = init_info.infra;
-        this.land_profile  = init_info.land_profile;
-        this.rail_profile  = init_info.rail_profile;
-        this.infra_profile = init_info.infra_profile;
+    constructor(map:   {'num_c':  number,
+                        'num_r':  number,
+                        'land':   number[][],
+                        'rail':   Rail[],
+                        'infra':  Infra[] },
+                nstage: number,
+                stage: {'budget': number,
+                        'nconv':  number,
+                        'land_profile': any,
+                        'rail_profile': any,
+                        'infra_profile': any}[]) {
+        this.num_c = map.num_c;
+        this.num_r = map.num_r;
+        this.rail  = map.rail;
+        this.infra = map.infra;
+        this.land  = new Array(this.num_r * this.num_c);
+        this.for_any_idx((coord, x) => this.land[x] =  map.land[coord.r][coord.c]);
+
+        this.nstage      = nstage;
+        this.stage       = stage;
+        this.stage_count = -1;
+        this.update_stage();
+    }
+
+    update_stage (): void {
+        this.stage_count += 1;
+        this.budget        = this.stage[this.stage_count].budget;
+        this.budget_r      = this.stage[this.stage_count].budget;
+        this.nconv         = this.stage[this.stage_count].nconv;
+        this.nconv_r       = this.stage[this.stage_count].nconv;
+        this.land_profile  = this.stage[this.stage_count].land_profile;
+        this.rail_profile  = this.stage[this.stage_count].rail_profile;
+        this.infra_profile = this.stage[this.stage_count].infra_profile;
         this.comp_land_price();
     }
 
@@ -119,29 +158,72 @@ export class Map {
             f(this.infra[i]);
     }
 
-    build (state: State, coords: HCoord[], new_type: number): void {
+    comp_cost (state: State, coords: HCoord[], ntype: number): {"cost": number, "nconv": number} {
+        let cost:  number = 0;
+        let nconv: number = 0;
         if (state == State.Land) {
-            coords.map(coord => {
-                this.budget_r -= this.get_land_profile(coord).tax;
-                this.nconv_r  -= 1;
-                this.land[this.get_idx(coord)] = new_type});
+            coords.map(_ => {
+                cost  += this.land_profile[ntype].tax;
+                nconv += 1;});
+        }
+        if (state == State.Rail && coords.length > 1) {
+            let new_rail: Rail = new Rail(coords, ntype);
+            let coords_xy: Coord[] = coords.map(grid_to_hex_center);
+            const len_del: number[] = coords_xy.map((coord: Coord, idx: number) =>
+                (idx == 0)? 0: Math.sqrt(Math.pow(coord.x - coords_xy[idx - 1].x, 2)
+                                       + Math.pow(coord.y - coords_xy[idx - 1].y, 2)));
+            const len: number = len_del.reduce((acc: number, del: number) => acc + del) / sqrt3;
+            cost = len * this.get_rail_profile(new_rail).tax
+        }
+        if (state == State.Infra) {
+            let new_infra: Infra = new Infra(coords, ntype);
+            cost = coords.length * this.get_infra_profile(new_infra).tax
+        }
+        return {"cost": cost, "nconv": nconv};
+    }
+
+    build (state: State, coords: HCoord[], ntype: number): void {
+        if (state == State.Land) {
+            coords.map(coord => this.land[this.get_idx(coord)] = ntype);
         }
         if (state == State.Rail) {
             if (coords.length <= 1)
                 return
-            let new_rail: Rail = new Rail(coords, new_type);
-            const len_del: number[] = coords.map((coord: HCoord, idx: number) =>
-                (idx == 0)? 0: Math.sqrt(Math.pow(coord.r - coords[idx - 1].r, 2)
-                                       + Math.pow(coord.c - coords[idx - 1].c, 2)));
-            const len: number = len_del.reduce((acc: number, del: number) => acc + del);
+            let new_rail: Rail = new Rail(coords, ntype);
             this.rail = [...this.rail, new_rail];
-            this.budget_r -= len * this.get_rail_profile(new_rail).tax
         }
         if (state == State.Infra) {
-            let new_infra: Infra = new Infra(coords, new_type);
+            let new_infra: Infra = new Infra(coords, ntype);
             this.infra = [...this.infra, new_infra];
-            this.budget_r -= coords.length * this.get_infra_profile(new_infra).tax
         }
+        if (state == State.DelR) {
+            if (coords.length != 2) 
+                return;
+
+            let split_rail: Rail[] = [];
+            this.rail.map(item => {
+                const idx1 = item.coords.findIndex(x => HCoord_eq(x, coords[0]));
+                const idx2 = item.coords.findIndex(x => HCoord_eq(x, coords[1]));
+                if (idx1 != -1 && idx2 != -1) {
+                    const coords1 = item.coords.filter((_, idx: number) => idx <= Math.min(idx1, idx2));
+                    const coords2 = item.coords.filter((_, idx: number) => idx >= Math.max(idx1, idx2));
+                    item.coords   = coords1;
+                    split_rail    = [...split_rail, {"coords": coords2,
+                                                     "type":   item.type}];
+                }
+            })
+            this.rail = [...this.rail, ...split_rail].filter(x => x.coords.length > 0);
+        }
+        if (state == State.DelI) {
+            this.infra.map(item => item.coords = item.coords.filter(coord => 
+                !coords.some(x => HCoord_eq(x, coord))));
+            this.infra = this.infra.filter(x => x.coords.length > 0);
+        }
+        let res = this.comp_cost(state, coords, ntype);
+        let cost  = res.cost;
+        let nconv = res.nconv;
+        this.budget_r -= cost;
+        this.nconv_r  -= nconv;
     }
 
     comp_land_price (): void {
@@ -162,11 +244,14 @@ export class Canvas {
     a:     number;
     state: State;
     sel:   HCoord[];
+    log:   {"state":  State,
+            "coords": HCoord[],
+            "type":   Number}[];
 
 
-    constructor(canvas:      any,
-                trigger:     () => void,
-                map:         Map) {
+    constructor(canvas:  any,
+                trigger: () => void,
+                map:     Map) {
         this.canvas      = canvas;
         this.ctx         = canvas.getContext('2d');
         this.trigger     = trigger;
@@ -174,6 +259,7 @@ export class Canvas {
 
         this.init_canvas();
         this.set_state(State.Land);
+        this.log = [];
         this.canvas.addEventListener('click', (e: any) => this.click_h(e))
     }
 
@@ -190,25 +276,73 @@ export class Canvas {
                                   {'r': Math.floor(r + 1), 'c': Math.floor(c - 1 * (Math.floor(r + 1) % 2))},
                                   {'r': Math.floor(r + 1), 'c': Math.floor(c - 1 * (Math.floor(r + 1) % 2) + 1)}];
         const remain: number[] = coords.map((coord) => (c - coord.c) * (c - coord.c) + (r - coord.r) * (r - coord.r));
-        this.sel = [...this.sel, coords[remain.indexOf(Math.min(...remain))]];
+        const coord: HCoord    = coords[remain.indexOf(Math.min(...remain))];
+
+        this.update_sel(coord);
         this.trigger();
     }
 
+    update_sel (coord: HCoord): void {
+        if (this.state == State.Land || this.state == State.Infra) {
+            if (this.sel.some(x => HCoord_eq(x, coord)))
+                this.sel = this.sel.filter(x => !HCoord_eq(x, coord));
+            else
+                this.sel = [...this.sel, coord];
+        }
+        if (this.state == State.Rail) {
+            if (this.sel.length > 0 && HCoord_eq(this.sel[this.sel.length - 1], coord))
+                this.sel.pop();
+            else
+                this.sel = [...this.sel, coord];
+        }
+        if (this.state == State.DelR) {
+            if (this.sel.some(x => HCoord_eq(x, coord)))
+                this.sel = this.sel.filter(x => !HCoord_eq(x, coord));
+            else {
+                let flag: boolean = false;
+                if (this.sel.length == 0) {
+                    this.map.rail.map(item => {
+                        if (item.coords.some(x => HCoord_eq(x, coord)))
+                            flag = true;
+                    });
+                }
+                if (this.sel.length == 1) {
+                    this.map.rail.map(item => {
+                        let idx1 = item.coords.findIndex(x => HCoord_eq(x, this.sel[0]));
+                        let idx2 = item.coords.findIndex(x => HCoord_eq(x, coord));
+                        if (idx1 != -1 && idx2 != -1 && Math.abs(idx1 - idx2) == 1)
+                            flag = true;
+                    })
+                }
+                if (flag)
+                    this.sel = [...this.sel, coord];
+            }
+        }
+        if (this.state == State.DelI) {
+            const pre_len: number = this.sel.length;
+            this.sel = this.sel.filter((item: HCoord) => item.r != coord.r || item.c != coord.c);
+            if (this.sel.length == pre_len) {
+                let flag: boolean = false;
+                this.map.infra.map(item => {
+                    if (item.coords.some(x => HCoord_eq(x, coord)))
+                        flag = true;
+                })
+                if (flag)
+                    this.sel = [...this.sel, coord];
+            }
+        }
+    }
+
     init_canvas () {
-        this.a = window.innerHeight / (this.map.num_c + 10);
-        this.canvas.width  = (this.map.num_c + 1) * this.a * sqrt3;
-        this.canvas.height = (this.map.num_r + 1) * this.a * 1.5;
+        this.a = Math.min((window.innerHeight)      / ((this.map.num_r + 2) * 1.5),
+                          (window.innerWidth - 300) / ((this.map.num_c + 2) * sqrt3));
+        this.canvas.width  = (this.map.num_c + 1.5) * this.a * sqrt3;
+        this.canvas.height = (this.map.num_r + 1.5) * this.a * 1.5;
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
     }
 
-    grid_to_hex_center (coord: HCoord): Coord {
-        const x: number = sqrt3 * coord.c + sqrt3 + sqrt3_2 * (coord.r % 2);
-        const y: number = 1.5   * coord.r + 1.5;
-        return {'x': x, 'y': y};
-    }
-
     draw_hex (coord: HCoord, draw_type: string): void {
-        const center     = this.grid_to_hex_center(coord)
+        const center     = grid_to_hex_center(coord)
 
         this.ctx.beginPath();
         this.ctx.moveTo(this.a * (center.x + delta_x[5]), this.a * (center.y + delta_y[5]));
@@ -240,7 +374,7 @@ export class Canvas {
     }
 
     draw_rail (rail: Rail): void {
-        const center: Coord[] = rail.coords.map((coord: HCoord) => this.grid_to_hex_center(coord));
+        const center: Coord[] = rail.coords.map((coord: HCoord) => grid_to_hex_center(coord));
 
         if (center.length == 0)
             return
@@ -249,9 +383,9 @@ export class Canvas {
         this.ctx.moveTo(this.a * center[0].x, this.a * center[0].y);
         center.map((coord: Coord) => this.ctx.lineTo(this.a * coord.x, this.a * coord.y));
 
-        this.ctx.lineWidth   = 3;
+        this.ctx.lineWidth   = this.map.get_rail_profile(rail).width;
         this.ctx.strokeStyle = this.map.get_rail_profile(rail).color;
-        this.ctx.setLineDash([])
+        this.ctx.setLineDash(this.map.get_rail_profile(rail).dash)
         this.ctx.stroke()
 
         if (rail.type == 0)
@@ -259,20 +393,23 @@ export class Canvas {
     }
 
     draw_infra (infra: Infra): void {
-        const center: Coord[] = infra.coords.map((coord: HCoord) => this.grid_to_hex_center(coord));
+        const center: Coord[] = infra.coords.map((coord: HCoord) => grid_to_hex_center(coord));
 
         if (center.length == 0)
             return
         
         center.map((coord: Coord) => {
-            this.ctx.beginPath();
-            console.log(coord.x, coord.y);
-            this.ctx.arc(this.a * coord.x, this.a * coord.y, 0.5 * this.a, 0, 2 * Math.PI, true);
-            this.ctx.closePath();
-            this.ctx.fillStyle   = this.map.get_infra_profile(infra).color;
-            this.ctx.fill()
-            //this.ctx.lineWidth   = 0;
-            //this.ctx.stroke()
+            this.ctx.fillStyle = this.map.get_infra_profile(infra).color;
+            if (this.map.get_infra_profile(infra).text == " ") {
+                this.ctx.beginPath();
+                this.ctx.arc(this.a * coord.x, this.a * coord.y, 0.5 * this.a, 0, 2 * Math.PI, true);
+                this.ctx.closePath();
+                this.ctx.fill()
+            }
+            else {
+                this.ctx.font = this.a + "px Sans";
+                this.ctx.fillText(this.map.get_infra_profile(infra).text, this.a * (coord.x - 0.5), this.a * (coord.y + 0.4));
+            }
         })
     }
 
@@ -289,16 +426,62 @@ export class Canvas {
             this.draw_rail(new Rail(this.sel, 0));
         if (this.state == State.Infra)
             this.sel.map(coord => this.draw_hex(coord, 'e'));
+        if (this.state == State.DelR) {
+            if (this.sel.length == 0) {
+                this.map.rail.map(item => item.coords.map(coord => this.draw_hex(coord, 'r') ));
+            }
+            if (this.sel.length == 1) {
+                this.draw_hex(this.sel[0], 'e');
+                this.map.rail.map(item => {
+                    let idx = item.coords.findIndex(x => HCoord_eq(x, this.sel[0]))
+                    if (idx != -1 && idx != 0)
+                        this.draw_hex(item.coords[idx - 1], 'r');
+                    if (idx != -1 && idx != item.coords.length - 1)
+                        this.draw_hex(item.coords[idx + 1], 'r');
+                })
+            }
+            if (this.sel.length == 2) {
+                this.draw_hex(this.sel[0], 'e');
+                this.draw_hex(this.sel[1], 'e');
+            }
+        }
+        if (this.state == State.DelI) {
+            this.map.infra.map(item => item.coords.map(coord => {
+                if (!this.sel.some(x => HCoord_eq(x, coord)))
+                    this.draw_hex(coord, 'r');
+                else
+                    this.draw_hex(coord, 'e');
+            }));
+        }
     }
 
-    set_state(new_state: State): void {
+    set_state (new_state: State): void {
         this.state = new_state;
         this.sel   = [];
         this.trigger();
     }
 
-    build(new_type: number): void {
-        this.map.build(this.state, this.sel, new_type);
+    comp_cost (ntype: number): {cost: number, nconv: number} {
+        return this.map.comp_cost(this.state, this.sel, ntype);
+    }
+
+    build (ntype: number): void {
+        this.map.build(this.state, this.sel, ntype);
+        this.log = [...this.log, {"state":  this.state,
+                                  "coords": this.sel, 
+                                  "type":   ntype}];
+        this.set_state(this.state);
+    }
+
+    get_log (): String {
+        return JSON.stringify(this.log);
+    }
+
+    update_stage (): void {
+        this.log = [...this.log, {"state":  State.Stage,
+                                  "coords": [],
+                                  "type":   0}];
+        this.map.update_stage();
         this.set_state(this.state);
     }
 }
